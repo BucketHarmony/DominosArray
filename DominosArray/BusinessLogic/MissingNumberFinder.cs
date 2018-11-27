b695ee9bf744f0126e5c028773b97dcc5bf8c1e6
Ken Raisor <ken.raisor@toyota.com>
2018-11-26 19:49:24 -0500

    Initial commit, including missing number finder and controller to illustrate it

---
+++ DominosArray/BusinessLogic/MissingNumberFinder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominosArray.BusinessLogic
+{
+    class MissingNumberFinder
+    {
+
+        public int Find(List<int> NumberList)
+        {
+            // Confirm that the array is not empty
+            if (!NumberList.Any()) { throw new ArgumentException("Array should not be empty."); }
+            
+            // Sort the array
+            List<int> SortedList = NumberList.OrderBy(i => i).ToList();
+
+            // Get highest and Lowest
+            int highestInt = SortedList[SortedList.Count - 1];
+            int lowestInt = SortedList[0];
+            int count = SortedList.Count;
+
+            // confirm that there is only one missing number
+            if((highestInt - lowestInt) != count) { throw new ArgumentException("Array should not have exacly 1 missing number in array."); }
+
+            // use Enumerable magic to find missing integer(s)
+            List<int> resultList = Enumerable.Range(lowestInt, count).Except(NumberList).ToList();
+
+            // confirm that there is only one missing integer
+            if (resultList.Count >= 2) { throw new ArgumentException("Array should not have more than 1 missing number in array."); }
+
+            // return the results
+            return resultList[0];
+        }
+    }
+}
--- DominosArray/Program.cs
+++ DominosArray/Program.cs
@@ -3,13 +3,92 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DominosArray.BusinessLogic;
 
 namespace DominosArray
 {
     class Program
     {
+        static MissingNumberFinder Finder = new MissingNumberFinder();
+
         static void Main(string[] args)
         {
+            var listOne = new List<int> { 1, 2, 4 };
+            ProcessAndDisplayArray(listOne);
+
+            var listTwo = new List<int> { 1, 2, 3, 4, 5, 6, 8, 9 };
+            ProcessAndDisplayArray(listTwo);
+
+            var listThree = new List<int> { 1, -1 };
+            ProcessAndDisplayArray(listThree);
+
+            var listFour = new List<int> { 8, 6, 7, 5, 3, 1, 9, 4 };
+            ProcessAndDisplayArray(listFour);
+
+            var listFive = new List<int> { 1, 0, -1, -3, -4};
+            ProcessAndDisplayArray(listFive);
+
+            try
+            {
+                var listEmpty = new List<int> {};
+                ProcessAndDisplayArray(listEmpty);
+            }
+            catch(Exception ex)
+            {
+                DisplayError(ex.Message);
+            }
+
+            try
+            {
+                var listSkipTwo = new List<int> { 1, 2, 3, 4, 7, 8 };
+                ProcessAndDisplayArray(listSkipTwo);
+            }
+            catch(Exception ex)
+            {
+                DisplayError(ex.Message);
+            }
+
+            try
+            {
+                var listMissingNone = new List<int> { 1, 2, 3, 4, 5 };
+                ProcessAndDisplayArray(listMissingNone);
+            }
+            catch (Exception ex)
+            {
+                DisplayError(ex.Message);
+            }
+
+            Console.WriteLine("Press any key to continue");
+            Console.Read();
+        }
+
+        private static void ProcessAndDisplayArray(List<int> NumberList)
+        {
+            DisplayArray(NumberList);
+            int result = Finder.Find(NumberList);
+            DisplayResults(result);
+        }
+
+        private static void DisplayArray(List<int> NumberList)
+        {
+            Console.WriteLine("Testing the following array of integers:");
+            foreach (var item in NumberList)
+            {
+                Console.Write("{0} ", item.ToString());
+            }
+            Console.WriteLine();
+        }
+
+        private static void DisplayResults(int MissingNumber)
+        {
+            Console.WriteLine("The missing number was: {0} ", MissingNumber);
+            Console.WriteLine();
+        }
+        private static void DisplayError(string Message)
+        {
+            Console.WriteLine("This array did not work");
+            Console.WriteLine(Message);
+            Console.WriteLine();
         }
     }
 }
